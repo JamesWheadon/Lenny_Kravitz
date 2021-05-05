78160a58ea07b0abe2d1492099a0f46fa99b18de
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-05 21:09:15 +0100

    update color, stying and positioning

--- client/src/layout/Header/index.js
+++ client/src/layout/Header/index.js
@@ -6,8 +6,8 @@ import './style.css'
 const Header = () => {
     return (
         <nav role="navigation">
-            <NavLink exact to="/" activeClassName="current">Home</NavLink>
-            <NavLink to="/albums" activeClassName="current">Albums</NavLink>
+            <NavLink exact to="/" activeClassName="current">HOME</NavLink>
+            <NavLink to="/albums" activeClassName="current">ALBUMS</NavLink>
         </nav>
     );
 }
